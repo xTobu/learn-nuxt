d8cf142986c2d3e5208984d25892e03c2b20ac17
Junxiang <junxiang.oriint@gmail.com>
2018-03-29 11:27:16 +0800

    update gopher.vue ( bind img src )

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,24 @@ module.exports = {
   ** Build configuration
   */
   build: {
+  //   loaders: [
+  //     {
+  //   test: /\.(png|jpe?g|gif|svg)$/,
+  //   loader: 'url-loader',
+  //   query: {
+  //     limit: 1000, // 1kB
+  //     name: 'img/[name].[hash:7].[ext]'
+  //   }
+  // },
+  // {
+  //   test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+  //   loader: 'url-loader',
+  //   query: {
+  //     limit: 1000, // 1kB
+  //     name: 'fonts/[name].[hash:7].[ext]'
+  //   }
+  // }
+  //   ],
     /*
     ** Run ESLint on save
     */
---
+++ pages/gopher.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="index">
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      <img v-bind:src="item"  alt="">
+    </li>
+  </ul>
+  <img  v-bind:src="gop"  alt="">
+  <img src="~/assets/img/gopher.png">
+</div>
+</template>
+
+<script>
+export default {
+  components: {},
+
+  asyncData(context) {
+    return {
+      items: [require('~/assets/img/gopher.png'),require('~/assets/img/gopher.png')],
+      gop:'img/gopher.png',
+      src: "gopher.png"
+    };
+  },
+  // method:{
+  //    imgPath(url) {
+  //      console.log(this.src)
+  //     return require("~/assets/img/"+"gopher.png");
+  //   },
+  // },
+
+  computed: {
+    imgPath(aa) {
+      return require("~/assets/img/" + "gopher.png");
+    }
+    // imagePath: function() {
+    //   return require("~/assets/" + this.src);
+    // }
+  },
+  beforeMount() {},
+  mounted() {
+    console.log(this.src);
+  }
+};
+</script>
+
+<style >
+
+</style>
